b41aea49e079d36417b1cff49f216cace62f73ee
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-05-17 11:28:40 +0100

    TestUnitCommit

--- src/test/java/recruitmentplatform/demo/DemoApplicationTests.java
+++ src/test/java/recruitmentplatform/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package recruitmentplatform.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void shouldReturnText() throws Exception {
+        String expected = "witajSwiecie";
+        String received = "witajSwiecie";
+        Assertions.assertEquals(received, expected);
     }
-
 }
